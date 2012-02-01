00be30153bf6a813ef57386d6a8354ec7ae8989c
tdroL <stdrol@gmail.com>
2012-02-01 11:12:54 +0100

    Added "primary" field type

---
+++ classes/fomg/field/primary.php
@@ -0,0 +1,3 @@
+<?php defined('SYSPATH') or die('No direct access allowed.');
+
+class Fomg_Field_Primary extends Kohana_Fomg_Field_Primary {}
---
+++ classes/kohana/fomg/field/primary.php
@@ -0,0 +1,11 @@
+<?php defined('SYSPATH') or die('No direct access allowed.');
+
+class Kohana_Fomg_Field_Primary extends Fomg_Field {
+
+	public function render(array $attr = array())
+	{
+		$name = $this->field->name;
+
+		return Form::hidden($name, $this->model->__get($name), $attr);
+	}
+}
